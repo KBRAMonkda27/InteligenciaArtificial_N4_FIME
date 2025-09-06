cd28b82a5e20750e52a02beb14c7cad86ad05991
KBRAMonkda27 <carlos.moncadac@uanl.edu.mx>
2025-09-05 21:28:57 -0600

    Delete ACTIVIDADES/AF5_Procesamiento_de_datos.py

--- ACTIVIDADES/AF5_Procesamiento_de_datos.py
+++
@@ -1,8 +0,0 @@
-#ACTIVIDAD 5: Procesamiento de conjunto de datos en Python
-#Equipo numero 5
-#N4 Carlos Adrian Moncada Chaires Matricula: 2173870
-#N4
-#Instrucciones.- Ejercicio 1.- Análisis y normalización de datos de sensores en un robot móvil
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
