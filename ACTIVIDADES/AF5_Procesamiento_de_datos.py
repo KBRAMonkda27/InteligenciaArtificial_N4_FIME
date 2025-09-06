fed89379c1e2d0c081a704da2000d6f11b56d123
KBRAMonkda27 <carlos.moncadac@uanl.edu.mx>
2025-09-05 19:26:18 -0600

    Update AF5_Procesamiento_de_datos.py

--- ACTIVIDADES/AF5_Procesamiento_de_datos.py
+++ ACTIVIDADES/AF5_Procesamiento_de_datos.py
@@ -2,4 +2,7 @@
 #Equipo numero 5
 #N4 Carlos Adrian Moncada Chaires Matricula: 2173870
 #N4
-import 
+#Instrucciones.- Ejercicio 1.- Análisis y normalización de datos de sensores en un robot móvil
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
